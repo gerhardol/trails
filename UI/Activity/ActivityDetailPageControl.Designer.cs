d13f8ba987f5bc9aef1bdee0d6eb14ad0d604cf9
gerhard.nospam <gerhard.nospam@localhost>
2010-07-31 00:46:20 +0000

    Update translation Bump version number Expand button lost

--- UI/Activity/ActivityDetailPageControl.Designer.cs
+++ UI/Activity/ActivityDetailPageControl.Designer.cs
@@ -436,7 +436,7 @@
             this.btnExpand.HyperlinkStyle = false;
             this.btnExpand.ImageMargin = 2;
             this.btnExpand.LeftImage = null;
-            this.btnExpand.Location = new System.Drawing.Point(483, 1);
+            this.btnExpand.Location = new System.Drawing.Point(353, 1);
             this.btnExpand.Name = "btnExpand";
             this.btnExpand.Padding = new System.Windows.Forms.Padding(2);
             this.btnExpand.PushStyle = false;

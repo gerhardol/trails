f0b7503568e4c597f8062880426aa07cf85836bd
gerhard.nospam <gerhard.nospam@localhost>
2011-01-29 18:58:05 +0000

    gps-running-issue-21 Italian translation

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.5.243.*")]
+[assembly: AssemblyVersion("0.5.244.*")]
 [assembly: AssemblyFileVersion("0.5")]
 [assembly: NeutralResourcesLanguageAttribute("")]

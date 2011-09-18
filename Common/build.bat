133db2dad7a1d0c11033723a150dd25995ce5624
gerhard.nospam <gerhard.nospam@localhost>
2011-09-18 22:20:42 +0000

    Moved building of stplugin package to a separate file Removed ST2 target visibility from solution

---
+++ Common/build.bat
@@ -0,0 +1,93 @@
+REM Copyright (C) 2011 Gerhard Olsson
+REM
+REM This library is free software; you can redistribute it and/or
+REM modify it under the terms of the GNU Lesser General Public
+REM License as published by the Free Software Foundation; either
+REM version 3 of the License, or (at your option) any later version.
+REM
+REM This library is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+REM Lesser General Public License for more details.
+REM
+REM You should have received a copy of the GNU Lesser General Public
+REM License along with this library. If not, see <http://www.gnu.org/licenses/>.
+
+REM $(PluginId)
+SET guid=%1
+REM $(StPluginVersion)
+SET StPluginVersion=%2
+REM ProjectName)
+SET ProjectName=%3
+REM $(ProjectDir)
+SET ProjectDir=%4
+REM $(StPluginPath)
+SET StPluginPath=%5
+REM $(TargetDir)
+SET TargetDir=%6
+REM $(ConfigurationType)
+SET ConfigurationType=%7
+REM Environment variable: stPlgoutdir
+
+REM Set the plugin GUID in the main PropertyGroup for the plugin
+IF "%guid%"=="" GOTO END
+
+REM ST version, for plugin.xml file
+SET StVersion=3.0.4205
+IF "%StPluginVersion%"=="2" SET StVersion=2.1.3478
+
+REM 7-zip must be configured, hardcoded path used
+REM "%programfiles%\7-zip\7z.exe"
+
+REM Plugin version
+set PluginVersion=0.1
+set perl=C:\Cygwin\bin\perl.exe
+IF NOT EXIST "%perl%" GOTO endversion 
+set cygwin=nodosfilewarning
+set tempfile=%temp%\%ProjectName%-stpluginversion.tmp
+%perl% -ne "if(/^^\[assembly: AssemblyVersion\(.([\.\d]*)(\.\*)*.\)\]/){print $1;}" %ProjectDir%\Properties\AssemblyInfo.cs > %tempfile%
+set /p PluginVersion= < %tempfile%
+rem DEL %tempfile%
+:endversion
+
+set stPlgFile=%ProjectDir%%ProjectName%-%PluginVersion%.st%StPluginVersion%plugin
+REM To move a .stplugin to common area, create environment variable (or set it below)
+REM set stPlgoutdir=g:\Users\go\dev\web
+
+REM Vista+ / XP compatibility
+set C_APPDATA=%PROGRAMDATA%
+IF "%C_APPDATA%"=="" set C_APPDATA=%ALLUSERSPROFILE%\Application Data
+IF EXIST "%C_APPDATA%" GOTO COPY_REL
+GOTO END_COPY_REL
+:COPY_REL
+set StTarget="%C_APPDATA%\%StPluginPath%\Update\%guid%\%ProjectName%"
+IF NOT EXIST %StTarget% mkdir %StTarget%
+
+REM XCOPY depreciated in Vista, use for XP compatibility
+XCOPY  "%TargetDir%*.*" %StTarget% /I/Y/Q/E <NUL:
+:END_COPY_REL
+
+REM generate the plugin.xml file
+ECHO ^<?xml version="1.0" encoding="utf-8" ?^> >  "%TargetDir%plugin.xml"
+ECHO ^<plugin id="%guid%" minimumCommonVersion="%StVersion%" /^> >> "%TargetDir%plugin.xml"
+
+rem IF "%ConfigurationType%"=="Debug" GOTO DebugPluginPackage
+
+DEL "%stPlgFile%"
+IF NOT EXIST "%programfiles%\7-zip\7z.exe" GOTO END
+"%programfiles%\7-zip\7z" a -r -tzip "%stPlgFile%" "%TargetDir%*" -x!*.st*plugin -x!*.tmp -x!*.locked -x!%ProjectName%.xml
+rem "%programfiles%\7-zip\7z" a -r -tzip "%stPlgFile%" "%TargetDir%*" -x!*.st*plugin -x!*.tmp -x!*.pdb -x!*.locked -x!%ProjectName%.xml
+
+IF "%stPlgoutdir%"=="" GOTO END
+IF not EXIST "%stPlgoutdir%" GOTO END
+COPY "%stPlgFile%" "%stPlgoutdir%"
+GOTO end
+
+:DebugPluginPackage
+GOTO end
+REM Create debug package
+DEL "%stPlgFile%"
+IF NOT EXIST "%programfiles%\7-zip\7z.exe" GOTO END
+"%programfiles%\7-zip\7z" a -r -tzip "%stPlgFile%" "%TargetDir%*" -x!*.st*plugin -x!*.tmp -x!*.locked -x!%ProjectName%.xml
+
+:END
